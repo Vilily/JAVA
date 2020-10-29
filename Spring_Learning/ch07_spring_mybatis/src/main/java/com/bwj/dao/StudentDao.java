b8033e78366ff98fc7f549d6c9d0a7fc22e23bc7
baowj-678 <bwj_678@qq.com>
2020-10-29 17:06:00 +0800

    mybatis

---
+++ Spring_Learning/ch07_spring_mybatis/src/main/java/com/bwj/dao/StudentDao.java
@@ -0,0 +1,10 @@
+package com.bwj.dao;
+
+import com.bwj.entity.Student;
+
+import java.util.List;
+
+public interface StudentDao {
+    int insertStudent(Student student);
+    List<Student> selectStudents();
+}
